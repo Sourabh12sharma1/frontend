75c9a013ec9f764a4b40d614c1d58d8a5a9b4658
Sourabh12sharma1 <Sourabh12sharma1@gmail.com>
2023-03-30 18:48:30 +0530

    first commit

--- text.js
+++ text.js
@@ -3,7 +3,7 @@ console.log(2+2);
 var fs  = require("fs")
 
 fs.writeFile(__dirname+"/index1.html","</h1>html is great </h1>",function(error){
-    if (error){
+    if (error){/
         return console.log(error)
     }else{
         return console.log("Congrats")
